a4997b84a3ffe60cf9d50893668e57984de1b703
Aarti Yadav <Aarti.Yadav@XEBIAINDIA.LOCAL>
2023-02-16 14:02:38 +0530

    markup added

--- lib/greetingcard.dart
+++ lib/greetingcard.dart
@@ -1,7 +1,18 @@
 library greetingcard;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class GreetingCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png'),
+          Text('Hello')
+        ],
+      ),
+    );
+  }
 }
